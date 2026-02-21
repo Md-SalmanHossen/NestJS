af5d215b5761e61a454d3752ff75d780aae51d40
SalmanHossen <hossainsalman873@gmail.com>
2026-02-21 11:06:35 +0600

    practice service in nestJS

--- nest-practice/src/product/product.controller.ts
+++ nest-practice/src/product/product.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Controller, Get, Param } from '@nestjs/common';
+import { ProductService } from './product.service';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+  constructor(private readonly productService:ProductService){}
+  @Get()
+  getProducts(){
+   return this.productService.getAllProducts();
+  }
+  @Get(':id')
+  getProduct(@Param('id') id:string){
+   return this.productService.getProductById(Number(id));
+  }
+}
