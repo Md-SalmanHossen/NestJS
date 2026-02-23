b8c6d913e5209ecfc438a7e2ee43f14c18ac218b
SalmanHossen <hossainsalman873@gmail.com>
2026-02-23 14:25:35 +0600

    practice dependency Injection

---
+++ nest-practice/src/category/category.controller.ts
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+import { Controller, Get } from '@nestjs/common';
+import { CategoryService } from './category.service';
+
+@Controller('category')
+export class CategoryController {
+   
+   constructor(private readonly categoryService:CategoryService){}
+   
+   @Get()
+   getAllCategory(){
+      return this.categoryService.getCategory();
+   }
+   
+}
