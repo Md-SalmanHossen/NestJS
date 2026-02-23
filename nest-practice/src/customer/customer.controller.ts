71a289536af1f0c93e5e3cb1da37e5539e69f161
SalmanHossen <hossainsalman873@gmail.com>
2026-02-23 17:49:09 +0600

    practice DTO and interface

---
+++ nest-practice/src/customer/customer.controller.ts
@@ -0,0 +1,19 @@
+/* eslint-disable prettier/prettier */
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CustomerService } from './customer.service';
+import { CreateCustomerDto } from './dto/create-customer.dto';
+
+@Controller('customer')
+export class CustomerController {
+   constructor(private readonly customerService:CustomerService){};
+
+   @Get()
+   getCustomer(){
+      return this.customerService.getAllCustomers();
+   }
+
+   @Post()
+   addCustomer(@Body() CreateCustomerDto:CreateCustomerDto){
+      return this.customerService.addCustomer(CreateCustomerDto);
+   }
+}
