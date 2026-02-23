c4f3e3402be08d40c0715a4da6010b54671b4a42
SalmanHossen <hossainsalman873@gmail.com>
2026-02-23 21:53:13 +0600

    practice DTO with class validator and transformer

--- nest-practice/src/customer/dto/create-customer.dto.ts
+++ nest-practice/src/customer/dto/create-customer.dto.ts
@@ -1,5 +1,10 @@
 /* eslint-disable prettier/prettier */
+import { IsInt, IsString } from "class-validator";
+
+/* eslint-disable prettier/prettier */
 export class CreateCustomerDto{
-   name:string;
+   @IsString()
+   name:string ;
+   @IsInt()
    age: number;
 }
