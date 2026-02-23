676bb56eee3bc7bd02238781ff209dae0ce6fa0e
SalmanHossen <hossainsalman873@gmail.com>
2026-02-23 16:25:00 +0600

    controller for student

--- nest-practice/src/student/student.controller.ts
+++ nest-practice/src/student/student.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Controller, Get,Param, Post,Body, Put, Patch, Delete } from '@nestjs/common';
+import { StudentService } from './student.service';
 
 @Controller('student')
-export class StudentController {}
+export class StudentController {
+   constructor(private readonly studentService:StudentService){};
+
+   @Get()
+   getAll(){
+      return this.studentService.getALlStudents();
+   }
+
+   @Get(':id')
+   getOne(@Param('id')id:string){
+      return this.studentService.getStudentById(Number(id));
+   }
+
+   @Post()
+   create(@Body() body:{name:string;age:number}){
+      return this.studentService.createStudent(body);
+   }
+
+   @Put(':id')
+   update(@Param('id') id:string,@Body() body:{name:string,age:number}){
+      return this.studentService.patchStudent(Number(id),body);
+   }
+
+   @Patch(':id')
+   Patch(@Param('id') id:string,@Body() body:Partial<{name:string,age:number}>){
+      return this.studentService.patchStudent(Number(id),body);
+   }
+
+   @Delete(':id')
+   remove(@Param('id') id:string){
+      return this.studentService.deleteStudent(Number(id));
+   }
+}
