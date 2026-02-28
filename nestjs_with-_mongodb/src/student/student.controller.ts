b9317c8989766260dfdce86a04b67f08b7615631
SalmanHossen <hossainsalman873@gmail.com>
2026-02-28 21:07:24 +0600

    insert data using monoose in nestjs

---
+++ nestjs_with-_mongodb/src/student/student.controller.ts
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+import { Body, Controller, Post } from '@nestjs/common';
+import { StudentService } from './student.service';
+import { Student } from './student.schema';
+
+@Controller('student')
+export class StudentController {
+   constructor(private readonly studentService:StudentService){}
+
+   @Post()
+   async addStudent(@Body() data:Partial<Student>){
+      return this.studentService.createStudent(data);
+   }
+   
+}
--- nestjs_with-_mongodb/src/student/student.schema.ts
+++ nestjs_with-_mongodb/src/student/student.schema.ts
@@ -2,7 +2,6 @@
 import { Prop,Schema,SchemaFactory } from "@nestjs/mongoose";
 import { Document } from "mongoose";
 
-
 export type StudentDocument = Student & Document;
 
 @Schema({timestamps:true}) 
