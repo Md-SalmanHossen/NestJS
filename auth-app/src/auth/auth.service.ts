e05261c78515b4c307a3883d272f709690044906
SalmanHossen <hossainsalman873@gmail.com>
2026-03-04 14:28:37 +0600

    done auth service

--- auth-app/src/auth/auth.service.ts
+++ auth-app/src/auth/auth.service.ts
@@ -1,7 +1,36 @@
-import { Injectable } from '@nestjs/common';
-import * as bcrypt from 'bcrypt';
+/* eslint-disable prettier/prettier */
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
+import *as bcrypt from 'bcrypt';
+
 
 @Injectable()
 export class AuthService {
+  constructor(
+   private userService:UsersService,
+   private jwtService:JwtService
+  ){}
+
+  async validateUser(email:string,pass:string){
   
+   const user=await this.userService.findByEmail(email);
+   if(!user) throw new UnauthorizedException();
+   
+   const isMatch=await bcrypt.compare(pass,user.password);
+   if(!isMatch) throw new UnauthorizedException();
+
+   return user;
+  }
+
+  async login(email:string,password:string){
+   
+   const user =await this.validateUser(email,password);
+   const payload={
+      sub:user._id,
+      email:user.email
+   }
+
+   return {access_token:this.jwtService.sign(payload)};
+  }
 }
